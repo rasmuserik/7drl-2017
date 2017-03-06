305e75bcf256a95e6d872d09113055c5c731b34c
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-03-06 07:50:47 +0100

    Commit 7drl-2017.js via https://appedit.solsort.com/?page=edit&github=solsort/7drl-2017

---
+++ 7drl-2017.js
@@ -0,0 +1,35 @@
+// # AppEdit Hello World
+//
+// Modules/libraries are loaded with `require(...)`.
+//
+// Documentation/source for the solsort library can be read on:
+// <https://appedit.solsort.com/?page=read&github=solsort/solsort>
+
+var ss = require('solsort');
+
+var cdnHost = "https://raw.githubusercontent.com/";
+var cdnUrl = cdnHost + "wesnoth/wesnoth/a9d014665673beb2bd4ad2c0d0e3a1f019e920bc/";
+var imgUrl = cdnUrl + "data/core/images/";
+var unitUrl = imgUrl + "units/";
+
+// Render the ui reactively
+
+ss.html(() => 
+  ['div',
+    ["img", {src: unitUrl + "dwarves/fighter.png"}],
+  ['h1', 'Hello world'],
+  ['p', 'Count: ', ss.getJS('count', 0)],
+  ['button', {onClick: ss.event('increment')},
+    'Increment']]);
+
+// Handler for button clicks
+
+ss.handle('increment', () => 
+  ss.setJS('count', ss.getJS('count', 0) + 1));
+
+// Information about the app, - used for exporting to github, etc.
+
+exports.info = {
+  name: 'Hello World',
+  github: 'solsort/7drl-2017'
+};

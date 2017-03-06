8b96c1ce4813b767421c05b965f624293c42b16b
Rasmus Jensen <moc.buhtig@solsort.dk>
2017-03-06 08:06:29 +0100

    Commit 7drl-2017.js via https://appedit.solsort.com/?page=edit&github=solsort/7drl-2017

--- 7drl-2017.js
+++ 7drl-2017.js
@@ -12,10 +12,54 @@ var cdnUrl = cdnHost + "wesnoth/wesnoth/a9d014665673beb2bd4ad2c0d0e3a1f019e920bc
 var imgUrl = cdnUrl + "data/core/images/";
 var unitUrl = imgUrl + "units/";
 
+var map = `
+w w w w w w w w w w w w w w w 
+ w w w w w w w w w w w w w w 
+w w w w w w w w w w w w w w w 
+ w w w w w w w w w w w w w w 
+w w w w w w w w w w w w w w w 
+ w w w w w w w w w w w w w w 
+w w w w w w w w w w w w w w w 
+ w w w w w w w w w w w w w w 
+`.split('\n').slice(1,-1);
+
+var terrain = {
+  g: {
+    img: 'grass/dry',
+    passable: true
+  },
+  w: {
+    img: 'swamp/water',
+    passable: false
+  },
+  s: {
+    img: 'sand/beach',
+    passable: false
+  },
+  m: {
+    img: 'mountains/basic',
+    passable: false
+  },
+  l: {
+    img:'unwalkable/lava',
+    passable: false,
+  }
+};
+
+function landscapeTiles() {
+  result = [];
+  for(var y = 0; y < map.length; ++y) {
+    for(x = y & 1; x < map[y].length; ++x) {
+      result.push(Object.assign({x:x, y:y}, makeTerrain(map[y][x])));
+    }
+  }
+  return result;
+}
 // Render the ui reactively
 
 ss.html(() => 
   ['div',
+   map[0][0],
     ["img", {src: unitUrl + "dwarves/fighter.png"}],
   ['h1', 'Hello world'],
   ['p', 'Count: ', ss.getJS('count', 0)],
